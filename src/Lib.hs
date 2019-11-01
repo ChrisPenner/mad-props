2564c5a915ce7eaca8ac493d1945fb77153f793e
Chris Penner <christopher.penner@gmail.com>
2019-11-01 11:34:36 -0600

    Almost set up and working, need to clean superpositions

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,24 +5,26 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveTraversable #-}
 module Lib where
 
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import Data.Graph.Inductive hiding ((&))
-import Data.Graph.Inductive.Graph hiding ((&))
 import qualified Data.Graph.Inductive.NodeMap as NM
-import Text.RawString.QQ (r)
+import qualified Text.RawString.QQ as R
 import Control.Monad
 import qualified Data.Map as M
 import qualified Data.Set as S
 import Data.Traversable
-import Data.Functor.Rep
+import Data.Foldable
+import Data.Functor.Rep as R
 import Data.Distributive
 import qualified Data.List as L
 import Data.Maybe
 import GraphLens
-import Control.Lens hiding (choice, Context)
+import Control.Lens hiding (Context)
 
 type Row = Int
 type Col = Int
@@ -37,6 +39,7 @@ data Dir
     | SW
     | W
     | NW
+    | C
     deriving (Eq, Show, Ord)
 
 type N loc val = (loc, val)
@@ -55,9 +58,10 @@ graphFromText txt = (numRows, numCols, coordMap, labeledGraph)
 mkGridGraph :: Row -> Col -> (NodeMap Coord, Gr Coord Dir)
 mkGridGraph rows cols = snd . NM.run empty $ do
     let slots = [(r, c) | r <- [0..rows - 1], c <- [0..cols - 1]]
-    for slots insMapNodeM
-    for slots $ \(r, c) -> do
+    for_ slots insMapNodeM
+    for_ slots $ \(r, c) -> do
         let addE = addEdge (r, c)
+        addE NW (r - 1, c - 1)
         addE N  (r - 1, c    )
         addE NE (r - 1, c + 1)
         addE E  (r    , c + 1)
@@ -90,11 +94,19 @@ rectangularize txt = (length lines', maxLength, padded)
     maxLength = maximum . fmap T.length $ lines'
     padded = T.justifyLeft maxLength ' ' <$> lines'
 
-txt :: T.Text
-txt = [r|one
+testText :: T.Text
+testText = [R.r|one
 a longer thing
 there
 |]
+
+simpleText :: T.Text
+simpleText =
+  [R.r|......
+......
+......
+|]
+
 
 when_ :: Monad m => Bool -> m a -> m ()
 when_ cond = when cond . void
@@ -106,20 +118,30 @@ instance Distributive Option where
   distribute = distributeRep
 
 instance Representable Option where
-  type Rep Option = Maybe Dir
-  index (Option{}) = undefined
+  type Rep Option = Dir
+  index (Option nw n ne w c e sw s se) d =
+      case d of
+          NW -> nw
+          N -> n
+          NE -> ne
+          E -> e
+          SE -> se
+          S -> s
+          SW -> sw
+          W -> w
+          C -> c
   tabulate f =
       Option
-       (f $ Just NW) (f $ Just N ) (f $ Just NE)
-       (f $ Just W ) (f $ Nothing) (f $ Just E )
-       (f $ Just SW) (f $ Just S ) (f $ Just SE)
+       (f NW) (f N) (f  NE)
+       (f W ) (f C) (f  E )
+       (f SW) (f S) (f  SE)
 
 data Option a =
     Option
       a a a
       a a a
       a a a
-  deriving (Show, Eq, Ord, Functor)
+  deriving (Show, Eq, Ord, Functor, Foldable, Traversable)
 
 collectSuperPositions :: Gr Char Dir -> S.Set Position
 collectSuperPositions gr
@@ -128,12 +150,11 @@ collectSuperPositions gr
       allNodes = nodes gr
       toPosition :: Node -> Position
       toPosition n =
-          let ctx = context gr n
-              linked = context gr n
+          let linked = context gr n
            in tabulate (findEdge linked)
-      findEdge :: Context Char Dir -> Maybe Dir -> Maybe Char
-      findEdge ctx Nothing = Just $ lab' ctx
-      findEdge ctx (Just dir) = do
+      findEdge :: Context Char Dir -> Dir -> Maybe Char
+      findEdge ctx C = Just $ lab' ctx
+      findEdge ctx dir = do
           (n, _) <- L.find ((== dir) . snd) $ lsuc' ctx
           lab gr n
 
@@ -160,10 +181,35 @@ minWavinessNode = fmap node' . ufold go Nothing
     entropyOf :: Context (SuperPos p) b -> Int
     entropyOf ctx = S.size (lab' ctx)
 
-solve :: Gr (SuperPos p) e -> Gr (SuperPos p) e
+solve :: Eq p => Gr (SuperPos (Option p)) Dir -> Gr (Option p) Dir
 solve gr = case minWavinessNode gr of
-    Nothing -> gr
-    Just n -> collapse undefined n gr
+    Nothing -> nmap pickFirst $ gr
+    Just n -> solve $ collapse gridFilter n gr
+
+pickFirst :: S.Set a -> a
+pickFirst = S.elemAt 0
+
+printOption :: Option (Maybe Char) -> String
+printOption o = unlines (fmap ((fromMaybe 'X') . R.index o) <$>
+    [ [NW, N, NE]
+    , [W,  C, E ]
+    , [SW, S, SE]])
+
+gridFilter :: (Eq a) => Option a -> Dir -> (Option a -> Bool)
+gridFilter choice dir other =
+    R.index choice dir
+      == R.index other (flipDir dir)
+
+flipDir :: Dir -> Dir
+flipDir NW = SE
+flipDir N = S
+flipDir NE = SW
+flipDir E = W
+flipDir SE = NW
+flipDir S = N
+flipDir SW = NE
+flipDir W = E
+flipDir C = C
 
 collapse :: forall p e. (p -> e -> (p -> Bool)) -> Node -> Gr (SuperPos p) e -> Gr (SuperPos p) e
 collapse nFilter n gr = newGraph
@@ -178,10 +224,18 @@ collapse nFilter n gr = newGraph
     choice = gr ^?! ctxAt n . ctxLabel . folded
     propFilter = nFilter choice
 
+simplePos =
+    let (rows, cols, srcNM, srcGr) = graphFromText $ simpleText
+     in collectSuperPositions srcGr
+
+
 test :: IO ()
 test = do
-    let (rows, cols, srcNM, srcGr) = graphFromText $ txt
+    let (rows, cols, srcNM, srcGr) = graphFromText $ simpleText
     let positions = collectSuperPositions srcGr
     -- T.putStrLn $ graphToText rows cols srcNM srcGr
-    let startGrid = generateGrid positions 10 10
+    let (destNM, startGrid) = generateGrid positions 10 10
+    let solved = nmap (fromJust . flip R.index C) $ solve startGrid
+    T.putStrLn $ graphToText 10 10 destNM solved
+
     return ()
