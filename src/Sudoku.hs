953bd74e0ce0b237dfbcabdba8678879d98fbd84
Chris Penner <christopher.penner@gmail.com>
2019-11-06 01:04:15 -0600

    Fully working sudoku solver

--- src/Sudoku.hs
+++ src/Sudoku.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeFamilies #-}
 module Sudoku where
 
 import WFC
@@ -6,28 +8,57 @@ import Control.Applicative
 import Control.Monad
 import Control.Monad.IO.Class
 import Data.Foldable
+import Data.Traversable
+import Control.Lens
 
 
-board :: [[Int]]
-board = [ x, x, x
-        , x, [3], x
-        , x, [6, 7], [6]]
+smolBoard :: [[Int]]
+smolBoard = [ x, x, x
+            , x, [3], x
+            , x, [6, 7], [6]]
   where
     x = [1..9]
 
--- board' :: [[Int]]
--- board' = [ x, x, x
---          , x, x, x
---          , x, x, x]
---   where
---     x = [1..9]
+fullBoard :: [[[Int]]]
+fullBoard = [[ x, x, x, x, x, x, x, x, x]
+            ,[x, x, x, x, x, x, x, x, x ]
+            ,[x, x, x, x, x, x, x, x, x ]
+            ,[x, x, x, x, x, x, x, x, x ]
+            ,[x, x, x, x, x, x, x, x, x ]
+            ,[x, x, x, x, x, x, x, x, x ]
+            ,[x, x, x, x, x, x, x, x, x ]
+            ,[x, x, x, x, x, x, x, x, x ]
+            ,[x, x, x, x, x, x, x, x, x ]]
+  where
+    x = [1..9]
 
+linkBoard :: [[PVar [] Int]] -> GraphM ()
+linkBoard xs = do
+    let coordPairs = do r <- [0..8]
+                        c <- [0..8]
+                        return (r, c)
+    for_ (liftA2 (,) coordPairs coordPairs) $ \(a, b) ->
+        when (sameRow a b || sameCol a b || sameBlock a b)
+            $ linkPair a b
+  where
+    linkPair (r, c) (r', c') = link (xs ^?! ix r . ix c) (xs ^?! ix r' . ix c') disjoint
+    sameRow (r, _) (r', _)  = r == r'
+    sameCol (_, c) (_, c')  = c == c'
+    sameBlock (r, c) (r', c') = (r `div` 3 == r' `div` 3) && (c `div` 3 == c' `div` 3)
 
-
+setup' :: GraphM ()
+setup' = do
+    vars <- (traverse . traverse) newPVar fullBoard
+    linkBoard vars
+    g <- getGraph
+    g' <- liftIO $ solve g
+    let results = fmap (flip readPVar g') <$> vars
+    liftIO $ print results
+    return ()
 
 setup :: GraphM ()
 setup = do
-    vars <- traverse newPVar board
+    vars <- traverse newPVar smolBoard
     traverse_ (\(a, b) -> link a b disjoint) $ do
         (a, b) <- (liftA2 (,) vars vars)
         guard (a /= b)
@@ -43,5 +74,5 @@ disjoint x = filter (/=x)
 
 run :: IO ()
 run = do
-    buildGraph setup
+    buildGraph setup'
     return ()
